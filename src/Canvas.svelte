ac5a8c1af288804e6c21d049691966339fb52c5f
Jan Willem Tulp <janwillemtulp@tulpinteractive.com>
2019-01-24 11:17:40 +0100

    using a Station class + more animation styling

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -32,48 +32,72 @@
           ctx.clearRect(0, 0, rect.width * dpr, rect.height * dpr)
           ctx.save()
 
+          legs.forEach(leg => leg.from.v.mix(leg.from.o, 0.0015))
+          activeLegs.forEach(activeLeg => activeLeg.leg.from.v.mix(activeLeg.leg.to.v, Math.log(1 + 0.015 * activeLeg.trips.reduce((acc, cur) => acc + (activeLeg.leg.dist / cur.duration), 0))))
+          // console.log(activeLegs[0].trips.reduce((acc, cur) => { acc + activeLegs[0].leg.dist / cur.duration}, 0))
+          // console.log(Math.log(1 + activeLegs[0].trips.reduce((acc, cur) => acc + (activeLegs[0].leg.dist / cur.duration), 0)))
           ctx.lineWidth = 1
           ctx.strokeStyle = '#333'
+          
+          /* globalCompositeOperation :
+            normal | multiply | screen | overlay | 
+            darken | lighten | color-dodge | color-burn | hard-light | 
+            soft-light | difference | exclusion | hue | saturation | 
+            color | luminosity
+          */
+          // ctx.globalCompositeOperation = 'multiply';
+          
 
           legs.forEach(leg => {
-            const p = new Path2D(leg.createPathString())
+            ctx.strokeStyle = '#333'
             ctx.setLineDash([])
-            ctx.stroke(p)
+            ctx.beginPath()
+            ctx.moveTo(leg.from.v.x, leg.from.v.y)
+            ctx.lineTo(leg.to.v.x, leg.to.v.y)
+            ctx.stroke()
+            ctx.closePath()
             
-            ctx.setLineDash([1, 1.5])
-            ctx.stroke(p)
+            // ctx.setLineDash([1, 1.5])
           })
+
+
+          // legs.forEach(leg => {
+          //   const p = new Path2D(leg.createPathString())
+          //   ctx.setLineDash([])
+          //   ctx.stroke(p)
+            
+          //   // ctx.setLineDash([1, 1.5])
+          //   // ctx.stroke(p)
+          // })
           
-          ctx.lineWidth = 1
-          ctx.strokeStyle = 'orange'
+          // ctx.lineWidth = 1
+          // ctx.strokeStyle = '#777'
 
-          activeLegs.forEach(leg => {
-            const p = new Path2D(leg.leg.createPathString())
-            ctx.setLineDash([])
-            ctx.stroke(p)
+          // activeLegs.forEach(leg => {
+          //   // ctx.strokeStyle = `hsla(${0 + leg.leg.from.v.distance(leg.leg.to.v) * 0.6}, 100%, ${30 + leg.leg.from.v.distance(leg.leg.to.v) * 0.3}%, 1)`
+          //   ctx.strokeStyle = 'rgba(255, 255, 255, 0.4)'
+          //   const p = new Path2D(leg.leg.createPathString())
+          //   ctx.setLineDash([])
+          //   ctx.stroke(p)
 
-            ctx.setLineDash([1, 1.5])
-            ctx.stroke(p)
-          })
+          //   // ctx.beginPath()
+          //   // ctx.moveTo(leg.leg.from.v.x, leg.leg.from.v.y)
+          //   // ctx.lineTo(leg.leg.to.v.x, leg.leg.to.v.y)
+          //   // ctx.stroke()
+          //   // ctx.closePath()
+
+          //   // ctx.setLineDash([1, 1.5])
+          //   // ctx.stroke(p)
+          // })
 
           ctx.fillStyle = '#fff'
           ctx.strokeStyle = '#000'
-          ctx.lineWidth = 2
-
+          ctx.lineWidth = 1
+          // console.log(stations[338].v.clone().subtract(stations[338].o).angleDeg(stations[338].v))
           stations.forEach(station => {
-            ctx.beginPath()
-            ctx.ellipse(
-              station.v.x,
-              station.v.y,
-              station.type.toLowerCase().indexOf('megastation') > -1 ? 3 : station.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
-              station.type.toLowerCase().indexOf('megastation') > -1 ? 3 : station.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
-              0,
-              0,
-              Math.PI * 2
-            )
-            ctx.stroke()
-            ctx.fill()
-            ctx.closePath()
+            station.drawOffsetPosition(ctx, station)
+            station.drawOffsetLine(ctx, station)
+            station.drawLabel(ctx, station)
           })
 
           ctx.restore()
---
+++ src/domain/Station.js
@@ -0,0 +1,77 @@
+import Victor from 'victor'
+
+export default class Station {
+  constructor(d, x, y) {
+    this.fullName = d['full-name']
+    this.shortName = d['short-name']
+    this.type = d['type']
+    this.x = x(+d.lon)
+    this.y = y(+d.lat)
+    this.o = new Victor(x(+d.lon), y(+d.lat))
+    this.v = new Victor(x(+d.lon), y(+d.lat))
+  }
+
+  drawLabel(ctx) {
+    if (['megastation', 'knooppuntIntercitystation'].includes(this.type)) {
+      ctx.fillStyle = '#fff'
+      ctx.textAlign = 'center'
+      ctx.fillText(this.fullName, this.v.x, this.v.y - 10)
+    }
+  }
+
+  drawOffsetPosition(ctx) {
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+    ctx.strokeStyle = '#000'
+    ctx.lineWidth = 1
+
+    ctx.beginPath()
+    ctx.ellipse(
+      this.v.x,
+      this.v.y,
+      this.type.toLowerCase().indexOf('megastation') > -1 ? 3 : this.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
+      this.type.toLowerCase().indexOf('megastation') > -1 ? 3 : this.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
+      0,
+      0,
+      Math.PI * 2
+    )
+    // ctx.stroke()
+    ctx.fill()
+    ctx.closePath()
+  }
+
+  drawOffsetLine(ctx) {
+    // ctx.strokeStyle = `hsla(0, 100%, ${20 + station.o.distance(station.v)}%, 1)`
+    // ctx.strokeStyle = 'rgba(255, 255, 255, 1)'
+    ctx.setLineDash([1, 1.5])
+    ctx.strokeStyle = `hsla(${this.v.clone().subtract(this.o).angleDeg(this.v)}, 50%, 50%, 0.7)`
+    // ctx.setLineDash([])
+
+    ctx.beginPath() // does not go right yet. stations are not updated
+    ctx.moveTo(this.o.x, this.o.y)
+    ctx.lineTo(this.v.x, this.v.y)
+    ctx.closePath()
+    ctx.stroke()
+  }
+
+  drawOriginalPosition(ctx) {
+    ctx.fillStyle = '#fff'
+    ctx.strokeStyle = '#000'
+    ctx.lineWidth = 1
+
+    ctx.beginPath()
+    ctx.ellipse(
+      this.o.x,
+      this.o.y,
+      // 1,
+      // 1,
+      this.type.toLowerCase().indexOf('megastation') > -1 ? 3 : this.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
+      this.type.toLowerCase().indexOf('megastation') > -1 ? 3 : this.type.toLowerCase().indexOf('knooppunt') > -1 ? 2 : 1,
+      0,
+      0,
+      Math.PI * 2
+    )
+    ctx.stroke()
+    ctx.fill()
+    ctx.closePath()
+  }
+}
